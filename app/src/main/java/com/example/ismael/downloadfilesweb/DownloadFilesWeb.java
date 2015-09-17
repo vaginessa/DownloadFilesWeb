64406ddbe0e1da70981ca419a0f51589912c1633
Ismael PONCE GORDILLO <gnuismo@gmail.com>
2015-09-17 07:19:08 +0000

    License

--- app/src/main/java/com/example/ismael/downloadfilesweb/DownloadFilesWeb.java
+++ app/src/main/java/com/example/ismael/downloadfilesweb/DownloadFilesWeb.java
@@ -40,6 +40,23 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/* DownloadFilesWeb download files referenced into web page.
+ * Copyright (C) 2015 Ismael PONCE GORDILLO
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 public class DownloadFilesWeb extends AppCompatActivity {
 
     private DownloadManager dManager;
